a1197bc726a6d0145ec26376d745020c440048c2
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2024-11-12 22:20:05 +0800

    forceTLS for pusher

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -15,7 +15,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: false
+    forceTLS: true
 });
 
 // @ts-ignore
