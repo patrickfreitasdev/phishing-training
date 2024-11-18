e5c9d02f21750fca196ab32c6c374154127207fa
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2024-11-18 08:09:15 +0800

    mobile improvements

--- resources/js/Pages/Hacked.tsx
+++ resources/js/Pages/Hacked.tsx
@@ -1,12 +1,14 @@
 export default function Hacked() {
     return (
         <div className="bg-red-500 min-h-screen flex items-center justify-center gap-6 p-3 flex-wrap">
-            <h1 className="text-8xl text-white font-bold text-center">
-                Hacked!
-                <small className="block text-4xl">
-                    You should pay more attention to the URLs you visit.
-                </small>
-            </h1>
+            <div className="max-w-screen-xl w-full">
+                <h1 className="text-8xl text-white font-bold text-center">
+                    Hacked!
+                    <small className="block text-4xl">
+                        You should pay more attention to the URLs you visit.
+                    </small>
+                </h1>
+            </div>
         </div>
     );
 }
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -6,31 +6,35 @@ import { Hourglass } from "react-loader-spinner";
 export default function Welcome() {
     return (
         <div className="flex flex-col h-screen">
-            <div className="bg-blue-500 min-h-[300px] flex items-center justify-center gap-6 p-3 flex-wrap">
-                <h1 className="text-6xl text-white font-bold">
-                    Join the game!
-                    <small className="block text-2xl">
-                        Scan the QR code, game ping 5256
-                    </small>
-                </h1>
-                <MyQRcode />
+            <div className="bg-blue-500 min-h-[300px]">
+                <div className="max-w-screen-xl w-full flex items-center justify-center gap-6 p-3 flex-wrap m-auto">
+                    <h1 className="text-6xl text-white font-bold">
+                        Join the game!
+                        <small className="block text-2xl">
+                            Scan the QR code, game pin 5256
+                        </small>
+                    </h1>
+                    <MyQRcode />
+                </div>
             </div>
-            <div className="bg-blue-600 flex-1 flex p-8 justify-between items-start">
-                <PlayersCount />
-                <img width={280} src={kahootImage} />
-                <div className="flex gap-4">
-                    <Hourglass
-                        visible={true}
-                        height="40"
-                        width="40"
-                        ariaLabel="hourglass-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        colors={["#ffff", "#72a1ed"]}
-                    />
-                    <span className="text-white text-3xl">
-                        Waiting for players
-                    </span>
+            <div className="bg-blue-600 flex-1 ">
+                <div className="flex p-8 justify-center lg:justify-between items-start max-w-screen-xl w-full m-auto flex-wrap gap-6">
+                    <PlayersCount />
+                    <img width={280} src={kahootImage} />
+                    <div className="flex gap-4">
+                        <Hourglass
+                            visible={true}
+                            height="40"
+                            width="40"
+                            ariaLabel="hourglass-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            colors={["#ffff", "#72a1ed"]}
+                        />
+                        <span className="text-white text-3xl">
+                            Waiting for players
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
